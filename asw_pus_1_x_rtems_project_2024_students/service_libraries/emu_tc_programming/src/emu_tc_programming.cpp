8b2a384eb79047e7f77052e3d18154cf9e732c9e
atcsol <atcsol@vmatc>
2024-03-12 17:43:06 +0100

    Practica 3. Primer Commit

--- asw_pus_1_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ asw_pus_1_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -10,18 +10,22 @@
 #include <public/emu_gss_v1.h>
 
 
-
+//Crea pruebas, TESTS, que programan telecomandos
 #define FT_SOLO_EPD_ICU_SERV_17_0010
 #define FT_SOLO_EPD_ICU_SERV_9_0020
 
 
-//TODO
-//#define FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
+
+#define FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
 
 
 #ifdef FT_SOLO_EPD_ICU_SERV_17_0010
 
+//SI utilizo esta clase, programo un telecomando de este tipo. La prueba solo tiene un paso (step_0)
+//Primer parametro es tiempo de liberacion de telecomando UNITIME_AFTER_POWER_ON + 6
+//Segundo parametro es un mensaje que aparece en simulación.
+//ASÍ EN ESE TIEMPO DE SIMULACION SE INYECTA UN TELECOMANDO DE SERVICIO 17, TELECOMANDO DE TEST
 EmuGSS_TCProgram17_1 prog_FT_0010_step_0(UNITIME_AFTER_POWER_ON + 6,
 					"FT_SOLO_EPD_ICU_SERV_17_0010 step 0, Connection Test");
 
@@ -32,6 +36,7 @@ EmuGSS_TCProgram17_1 prog_FT_0010_step_0(UNITIME_AFTER_POWER_ON + 6,
 
 #define FT_SOLO_EPD_ICU_SERV_9_0020_TIME 200000
 
+//En esta clase ademas del timepo de liberacion y el mensaje, se proporciona el nuevo tiempo de a abordo, TELECOMANDO DE TIEMPO DE A BORDO
 EmuGSS_TCProgram9_129 prog_FT_0020_step_0(UNITIME_AFTER_POWER_ON + 10 ,
 		"FT_SOLO_EPD_ICU_SERV_9_0020 step 0, Update UniTime Test",
 		FT_SOLO_EPD_ICU_SERV_9_0020_TIME);
@@ -39,9 +44,9 @@ EmuGSS_TCProgram9_129 prog_FT_0020_step_0(UNITIME_AFTER_POWER_ON + 10 ,
 
 
 #endif
-/*
-#ifdef FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
+//Pero recuerda que esta telemetría no está referenciada como válida though (la 17-3)
+#ifdef FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
 #define FT_SOLO_EPD_ICU_SERV_1_0030_TIME (FT_SOLO_EPD_ICU_SERV_9_0020_TIME + 2)
 
@@ -49,5 +54,5 @@ EmuGSS_TCProgram17_3 prog_FT_0030_step_0(FT_SOLO_EPD_ICU_SERV_1_0030_TIME,
 		"FT_SOLO_EPD_ICU_SERV_1_0030 step 0, Rejected TC",0xFF);
 
 #endif
-*/
+
 
